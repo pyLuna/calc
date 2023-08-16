116e0bf8e0b98ceb5a6dae98228bc6ca58fc6e6d
QCU <QCU@DESKTOP-7GI2R8Q>
2023-08-17 00:56:52 +0800

    checkpoint, might do revisions.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,37 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="calc">
+    <div class="calc-header">
+        <h1>Hello,World</h1>
+    </div>
+    <div class="calc-content">
+        <div class="calc-btn">
+            
+        </div>
+    </div>
+</div>
+
+<style>
+    .calc{
+        border-radius: 20px;
+        border-style: solid;
+        position: absolute;
+        top: 10%;
+        left: 35%;
+        height: 600px;
+        width:500px;
+    }
+    .calc-header{
+        border-radius: 20px;
+        position: relative;
+        border-style: solid;
+        color:red;
+        left: 25px;
+        top: 25px;
+        width: 450px;
+        height: 100px;
+        display: flex;
+    }
+    .calc-header h1{
+        position: relative;
+
+    }
+</style>
